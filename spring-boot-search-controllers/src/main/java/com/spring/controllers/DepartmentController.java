1cf1aed7cd379044418f64c44a3cbd23e4155c8c
rkpatra201 <rkpatra201@gmail.com>
2017-02-27 23:07:09 +0530

    Add files via upload

---
+++ spring-boot-search-controllers/src/main/java/com/spring/controllers/DepartmentController.java
@@ -0,0 +1,14 @@
+package com.spring.controllers;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.model.EmployeeModel;
+
+@RestController
+public class DepartmentController {
+
+	
+}
