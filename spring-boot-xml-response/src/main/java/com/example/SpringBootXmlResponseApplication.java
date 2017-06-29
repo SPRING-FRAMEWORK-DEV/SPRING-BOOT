8f79e0f2e965f2aa763cc1a9180701632b534a6a
rkpatra201 <rkpatra201@gmail.com>
2017-06-29 19:12:53 +0530

    spring boot xml response with gradle
    spring boot xml response with gradle

---
+++ spring-boot-xml-response/src/main/java/com/example/SpringBootXmlResponseApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootXmlResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootXmlResponseApplication.class, args);
+	}
+}
+// http://localhost:9898/student
+// http://localhost:9898/studentlist
---
+++ spring-boot-xml-response/src/main/java/com/example/springbootxmlresponse/SpringBootXmlResponseApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootxmlresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootXmlResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootXmlResponseApplication.class, args);
+	}
+}
